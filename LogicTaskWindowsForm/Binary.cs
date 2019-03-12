74290eec8c30ce68232f725d54c1a05c104ad90d
Ahmed Hafez <Ahmed41901@gmail.com>
2019-03-12 21:43:08 +0200

    Binary to hexa and octal was resolved

--- LogicTaskWindowsForm/Binary.cs
+++ LogicTaskWindowsForm/Binary.cs
@@ -21,67 +21,130 @@ namespace LogicTaskWindowsForm
             return dec;
         }
 
+        /// <summary>
+        /// Edit the specified <paramref name="binary"/> by adding
+        /// to it some leading zeors to make its length divisible
+        /// by <paramref name="numberOfBits"/>
+        /// </summary>
+        /// <param name="binary">The specified binary number</param>
+        /// <param name="numberOfBits">The number of bits to make the binary
+        /// number length divisible by it</param>
+        private string editBinary(string binary, int numberOfBits)
+        {
+            if (binary.Length % numberOfBits != 0)
+            {
+                int leadingZeros = numberOfBits - (binary.Length % numberOfBits);
+                string zeros = "";
+                for (int i = 0; i < leadingZeros; i++)
+                    zeros += "0";
+                binary = zeros + binary;
+            }
+            return binary;
+        }
+        
         public string BinToOct(string bin)
         {
+            // TODO Binary to Octal was edited
+            string octal = "";
+            bin = editBinary(bin, 3); // See Line 32
+            for (int i = bin.Length - 1; i >= 0; )
+            {
+                double right3Bits = 0;
+                for (int j = 0; j < 3; j++, i--)
+                    right3Bits += Math.Pow(2, j) * (bin[i] - '0');
+                octal = right3Bits.ToString() + octal;
+            }
+            return octal;
+
+
+
+
+
+
+
+
             //int itsDec = Convert.ToInt32(bin, 2);
             //return Convert.ToString(itsDec, 8);
 
+            
+            //int bit, binNumber;
+            //string threeBits = "", oct = "", octReversed = "";
 
-            // TODO Check this
-            int bit, binNumber;
-            string threeBits = "", oct = "", octReversed = "";
-            binNumber = int.Parse(bin);
-            while (binNumber != 0)
-            {
-                bit = binNumber % 10;
-                threeBits += bit;
-                if ((threeBits.Length % 3 == 0 && threeBits != "0"))
-                {
-                    oct += BinToDec(threeBits);
-                    threeBits = "";
-                }
-                if (binNumber == 10 || binNumber == 01 || binNumber == 11 || binNumber == 1)
-                {
-                    threeBits = binNumber.ToString();
-                    oct += BinToDec(threeBits);
-                    binNumber = 0;
-                }
-                binNumber /= 10;
-            }
-            for (int i = oct.Length - 1; i >= 0; --i)
-                octReversed += oct[i];
-            return octReversed;
+            //// بتحولى الباينرى لانتجر ... طب يرضى مين ده ؟
+            ////binNumber = int.Parse(bin);
+
+            //while (binNumber != 0)
+            //{
+            //    bit = binNumber % 10;
+            //    threeBits += bit;
+            //    if ((threeBits.Length % 3 == 0 && threeBits != "0"))
+            //    {
+            //        oct += BinToDec(threeBits);
+            //        threeBits = "";
+            //    }
+            //    if (binNumber == 10 || binNumber == 01 || binNumber == 11 || binNumber == 1)
+            //    {
+            //        threeBits = binNumber.ToString();
+            //        oct += BinToDec(threeBits);
+            //        binNumber = 0;
+            //    }
+            //    binNumber /= 10;
+            //}
+            //for (int i = oct.Length - 1; i >= 0; --i)
+            //    octReversed += oct[i];
+            //return octReversed;
         }
 
         public string BinToHex (string bin)
         {
-            //return Convert.ToInt32(bin, 2).ToString("x");
-
-            // TODO Check this
-            int bit, binNumber;
-            string fourBits = "", hex = "", hexReversed = "";
-            binNumber = int.Parse(bin);
-            while (binNumber != 0)
+            // TODO Binary to Hexadecimal was edited
+            string hexadecimal = "";
+            bin = editBinary(bin, 4); // See Line 32
+            for (int i = bin.Length - 1; i >= 0;)
             {
-                bit = binNumber % 10;
-                fourBits += bit;
-                if ((fourBits.Length % 4 == 0 && fourBits != "0"))
-                {
-                    hex += BinToDec(fourBits);
-                    fourBits = "";
-                }
-                if (binNumber == 10 || binNumber == 01 || binNumber == 11 || binNumber == 1)
-                {
-                    binNumber /= 10;
-                    fourBits = binNumber.ToString();
-                    hex += BinToDec(fourBits);
-                    binNumber = 0;
-                }
-                binNumber /= 10;
+                double right4Bits = 0;
+                for (int j = 0; j < 4; j++, i--)
+                    right4Bits += Math.Pow(2, j) * (bin[i] - '0');
+
+                // Note : The Unicode of char 'A' is 65 which equal = 10 + 55.
+                if (right4Bits > 9) hexadecimal = (char)(right4Bits + 55) + hexadecimal;
+                else hexadecimal = right4Bits.ToString() + hexadecimal;
             }
-            for (int i = hex.Length - 1; i >= 0; --i)
-                hexReversed += hex[i];
-            return hexReversed;
+            return hexadecimal;
+
+
+
+
+
+
+
+
+            //return Convert.ToInt32(bin, 2).ToString("x");
+            
+            //int bit, binNumber;
+            //string fourBits = "", hex = "", hexReversed = "";
+            //binNumber = int.Parse(bin);
+            //while (binNumber != 0)
+            //{
+            //    bit = binNumber % 10;
+            //    fourBits += bit;
+            //    if ((fourBits.Length % 4 == 0 && fourBits != "0"))
+            //    {
+            //        hex += BinToDec(fourBits);
+            //        fourBits = "";
+            //    }
+            //    if (binNumber == 10 || binNumber == 01 || binNumber == 11 || binNumber == 1)
+            //    {
+            //        binNumber /= 10;
+            //        fourBits = binNumber.ToString();
+            //        hex += BinToDec(fourBits);
+            //        binNumber = 0;
+            //    }
+            //    binNumber /= 10;
+            //}
+            //for (int i = hex.Length - 1; i >= 0; --i)
+            //    hexReversed += hex[i];
+            //return hexReversed;
         }
     }
 }
